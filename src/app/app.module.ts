a48437ca39f594978deb4438426b4308c46717e3
unknown <ak149649@gmal.com>
2024-07-19 16:38:34 +0530

    This is frontend on angular for mcqexam.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatFormFieldModule,MatButtonModule,MatInputModule,FormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+    public user={
+      username:'',
+      password:'',
+      firstName:'',
+      lastName:'',
+      email:'',
+      phone:''
+
+    }
+
+  registerFromSubmit(){
+    console.log(this.user);
+    if(this.user.username == '' || this.user.username == null){
+      alert('Username is required');
+      return;
+    }
+    alert("Registered");
+  }
+}
