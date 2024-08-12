a933a323452498e71c9de6149b39f74f7bd1d329
Ashish Kumar Gupta <78798108+ashishk-cmd@users.noreply.github.com>
2024-08-13 00:58:27 +0530

    Update QuestionService.java

--- src/main/java/com/exam/service/QuestionService.java
+++ src/main/java/com/exam/service/QuestionService.java
@@ -16,6 +16,7 @@ public interface QuestionService {
 	public Question getQuestion(Long questionId);
 	
 	public Set<Question> getQuestionsOfQuiz(Quiz quiz);
-	
+
+	public void deleteQuestion(Long questionId);
 	
 }
